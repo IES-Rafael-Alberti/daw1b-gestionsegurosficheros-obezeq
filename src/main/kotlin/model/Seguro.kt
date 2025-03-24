0ef922f38d9861553b83e904d006e291c8c8adc5
obezeq <eortbra2812@g.educaand.es>
2025-03-24 21:24:28 +0100

    seguro advancements

--- src/main/kotlin/model/Seguro.kt
+++ src/main/kotlin/model/Seguro.kt
@@ -1,5 +1,29 @@
 package org.example.model
 
-abstract class Seguro () {
+abstract class Seguro (
+    val numPoliza: Int,
+    private val dniTitular: String,
+    protected val importe: Double
+) : IExportable {
+
+    abstract fun calcularImporteAnioSiguiente(interes: Double): Double
+
+    abstract fun tipoSeguro(): String
+
+    override fun serializar(separador: String): String {
+        return "${numPoliza};${dniTitular};${importe}"
+    }
+
+    override fun toString(): String {
+        return "Seguro(numPoliza=${numPoliza}, dniTitular=${dniTitular}, importe=${importe})"
+    }
+
+    override fun hashCode(): Int {
+        return super.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return super.equals(other)
+    }
 
 }
